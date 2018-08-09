9b83aab6ec107f7ef9b11782c1a9b55040b746e0
Ales Sinkovec <ales@parity.solutions>
2018-08-09 13:49:45 -0300

    created the seeds file for ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+require 'open-uri'
+require 'json'
+
+
+puts "destroying all"
+Ingredient.destroy_all
+
+puts "seeding ingredients"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = open(url).read
+parsed_data = JSON.parse(data)
+parsed_data.drinks.each do |ingredient|
+  ingredient = Ingredient.new(name: igredient["strIngredient1"])
+  ingredient.save!
+end
+
+puts "completed"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
